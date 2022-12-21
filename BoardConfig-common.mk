ProtonAOSP/android_device_google_gs101

Build OpenCL NNAPI backend when blobs are present
Change-Id: Ib2c5176e4c95ca5f32c2c181ec33376437d38dc5

BoardConfig-common.mk
@@ -349,7 +349,7 @@ WIFI_FEATURE_HOSTAPD_11AX := true
 
 # NeuralNetworks
 GPU_SOURCE_PRESENT := $(wildcard vendor/arm/mali/valhall)
-GPU_PREBUILD_PRESENT := $(wildcard vendor/google_devices/gs101/prebuilts/gpu/libs)
+GPU_PREBUILD_PRESENT := $(wildcard vendor/google_devices/$(TARGET_DEVICE)/proprietary)
 ifneq "$(or $(GPU_SOURCE_PRESENT),$(GPU_PREBUILD_PRESENT))" ""
 ARMNN_COMPUTE_CL_ENABLE := 1
 else

CrookedAndroid/device_google_gs201

(cherry picked from commit 83f15395b93402e04398e6da4e22252907cd362f)

BoardConfig-common.mk
@@ -348,7 +348,7 @@ BOARD_EXYNOS_S10B_FORMAT_ALIGN := 64
 
 # NeuralNetworks
 GPU_SOURCE_PRESENT := $(wildcard vendor/arm/mali/valhall)
-GPU_PREBUILD_PRESENT := $(wildcard vendor/google_devices/zebu/prebuilts/libs)
+GPU_PREBUILD_PRESENT := $(wildcard vendor/google/$(TARGET_DEVICE)/proprietary)
 ifneq (,$(strip $(GPU_SOURCE_PRESENT) $(GPU_PREBUILD_PRESENT)))
 ARMNN_COMPUTE_CL_ENABLE := 1
 else